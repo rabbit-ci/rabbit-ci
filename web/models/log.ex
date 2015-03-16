6ddc50801713f200be5e4a7665577a6808e7a9f5
Theron Boerner <hunterboerner@gmail.com>
2015-03-16 17:59:33 -0500

    Add changeset to log

--- web/models/log.ex
+++ web/models/log.ex
@@ -5,4 +5,14 @@ defmodule Rabbitci.Log do
     field :stdio, :string
     timestamps
   end
+
+  @doc """
+  Creates a changeset based on the `model` and `params`.
+
+  If `params` are nil, an invalid changeset is returned
+  with no validation performed.
+  """
+  def changeset(model, params \\ nil) do
+    cast(model, params, ~w(status), ~w())
+  end
 end
