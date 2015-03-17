546d737ba2dcf665a58e9c0960e88c719259f3c0
Theron Boerner <hunterboerner@gmail.com>
2015-03-17 15:44:44 -0500

    Convert start_time and finish_time to ISO 8601 in serializers

--- web/serializers/build_serializer.ex
+++ web/serializers/build_serializer.ex
@@ -8,4 +8,9 @@ defmodule Rabbitci.BuildSerializer do
     Rabbitci.Build.script_ids(record)
   end
 
+  def start_time(%Rabbitci.Build{start_time: nil}), do: nil
+  def start_time(r), do: Ecto.DateTime.to_string(r.start_time)
+  def finish_time(%Rabbitci.Build{finish_time: nil}), do: nil
+  def finish_time(r), do: Ecto.DateTime.to_string(r.finish_time)
+
 end
