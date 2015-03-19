ea516804fac3da58a62cc6d712cc8c26692b66ea
Theron Boerner <hunterboerner@gmail.com>
2015-03-19 13:02:15 -0500

    Better index in controller

--- web/controllers/build_controller.ex
+++ web/controllers/build_controller.ex
@@ -1,18 +1,32 @@
 defmodule Rabbitci.BuildController do
   use Rabbitci.Web, :controller
 
+  import Ecto.Query
   alias Rabbitci.Build
 
   plug :scrub_params, "build" when action in [:create, :update]
   plug :action
 
-  def index(conn, _params) do
-    builds = Repo.all(Build)
+
+  def index(conn, %{"ids" => ids, "page" => page}) do
+    builds = Repo.all(from b in Build,
+                      where: b.id in ^ids,
+                      limit: 100,
+                      offset: ^(page * 100))
     conn
     |> assign(:builds, builds)
     |> render("index.json")
-    # This will probably need to be paginated.
   end
+
+  def index(conn, params = %{"ids" => _}) do
+    index(conn, Map.merge(params, %{"page" => 0}))
+  end
+
+  def index(conn, params) do
+    # I don't think this is standard. And where are the response codes?
+    json(conn, %{message: "Please provide an array of ids", status: "error"})
+  end
+
 
   def create(conn, %{"build" => build_params}) do
     changeset = Build.changeset(%Build{}, build_params)
