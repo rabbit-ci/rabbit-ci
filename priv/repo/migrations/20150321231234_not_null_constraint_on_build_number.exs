0cb2ac4ddb153e8b131a9cce6c4e4972305bbc39
Theron Boerner <hunterboerner@gmail.com>
2015-03-21 18:18:06 -0500

    Not null constraint on build_number

---
+++ priv/repo/migrations/20150321231234_not_null_constraint_on_build_number.exs
@@ -0,0 +1,11 @@
+defmodule Rabbitci.Repo.Migrations.NotNullConstraintOnBuildNumber do
+  use Ecto.Migration
+
+  def up do
+    execute("ALTER TABLE builds ALTER COLUMN build_number SET NOT NULL")
+  end
+
+  def down do
+    execute("ALTER TABLE builds ALTER COLUMN build_number DROP NOT NULL")
+  end
+end
