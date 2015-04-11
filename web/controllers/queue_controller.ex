3db7dec8eebedae07c42d8aeebb461ad0fb0f025
Theron Boerner <hunterboerner@gmail.com>
2015-04-11 18:26:30 -0500

    Index actions

--- web/controllers/queue_controller.ex
+++ web/controllers/queue_controller.ex
@@ -3,8 +3,13 @@ defmodule Rabbitci.QueueController do
 
   plug :action
 
-  def index(conn, _params) do
-    conn |> send_resp(200, "Kewl")
+  def index(conn, %{"repo" => repo, "commit" => commit}) do
+    Exq.enqueue(:exq, "workers", "ConfigExtractor", [repo, commit])
+    conn |> send_resp(200, "Queued")
+  end
+
+  def index(conn, _) do
+    conn |> send_resp(400, '"repo" or "commit" URL parameter missing.')
   end
 
 end
