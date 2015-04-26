31f618c6c2890663ba76e72248971f97a5c383fa
Theron Boerner <hunterboerner@gmail.com>
2015-04-26 16:58:22 -0500

    Change generate_a_lot_of_builds return value

--- test/controllers/build_controller_test.exs
+++ test/controllers/build_controller_test.exs
@@ -19,28 +19,28 @@ defmodule Rabbitci.BuildControllerTest do
       b.id
     end
 
-    {project.name, branch.name, ids}
+    {project, branch, ids}
   end
 
   test "page offset should default to 0" do
-    {project_name, branch_name, _} = generate_a_lot_of_builds
-    url = "/projects/#{project_name}/branches/#{branch_name}/builds"
+    {project, branch, _} = generate_a_lot_of_builds
+    url = "/projects/#{project.name}/branches/#{branch.name}/builds"
     response = get url
     body = Poison.decode!(response.resp_body)
     assert hd(body["builds"])["build_number"] == 40
   end
 
   test "page offset should work" do
-    {project_name, branch_name, _} = generate_a_lot_of_builds
-    url = "/projects/#{project_name}/branches/#{branch_name}/builds"
+    {project, branch, _} = generate_a_lot_of_builds
+    url = "/projects/#{project.name}/branches/#{branch.name}/builds"
     response = get url, [page: %{offset: "1"}]
     body = Poison.decode!(response.resp_body)
     assert hd(body["builds"])["build_number"] == 10
   end
 
   test "show a single build" do
-    {project_name, branch_name, ids} = generate_a_lot_of_builds
-    url = "/projects/#{project_name}/branches/#{branch_name}/builds/1"
+    {project, branch, ids} = generate_a_lot_of_builds
+    url = "/projects/#{project.name}/branches/#{branch.name}/builds/1"
     response = get url
     body = Poison.decode!(response.resp_body)
     assert is_map(body)
