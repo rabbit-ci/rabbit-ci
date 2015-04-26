302b71bf5d497e7da75ea31d2e77f40bc9431bd6
Theron Boerner <hunterboerner@gmail.com>
2015-04-26 16:58:01 -0500

    Test get_build

---
+++ test/controllers/helpers_test.exs
@@ -0,0 +1,28 @@
+defmodule Rabibtci.HelpersTest do
+  use Rabbitci.Integration.Case
+  use Rabbitci.TestHelper
+
+  alias Rabbitci.Repo
+  alias Rabbitci.Project
+  alias Rabbitci.Branch
+  alias Rabbitci.Build
+  alias Rabbitci.ControllerHelpers
+
+  test "get_build should provide correct build" do
+    time = Ecto.DateTime.utc()
+    project = Repo.insert(%Project{name: "blah", repo: "lala"})
+    branch1 = Repo.insert(%Branch{name: "branch1", project_id: project.id})
+    build1 = Repo.insert(%Build{branch_id: branch1.id, build_number: 1,
+                       start_time: time, finish_time: time})
+
+    branch2 = Repo.insert(%Branch{name: "branch2", project_id: project.id})
+    build2 = Repo.insert(%Build{branch_id: branch2.id, build_number: 1,
+                                start_time: time, finish_time: time})
+    build3 = Repo.insert(%Build{branch_id: branch2.id, build_number: 2,
+                                start_time: time, finish_time: time})
+
+    assert ControllerHelpers.get_build(branch1, "latest") == build1
+    assert ControllerHelpers.get_build(branch2, 1) == build2
+    assert ControllerHelpers.get_build(branch2, "latest") == build3
+  end
+end
