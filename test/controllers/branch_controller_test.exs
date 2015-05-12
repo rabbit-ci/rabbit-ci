2927d06f945a786b3bc947efc9b14b984ae38d8f
Theron Boerner <hunterboerner@gmail.com>
2015-05-12 16:53:03 -0500

    More tests

--- test/controllers/branch_controller_test.exs
+++ test/controllers/branch_controller_test.exs
@@ -14,10 +14,23 @@ defmodule Rabbitci.BranchControllerTest do
                           project_id: project.id}
     end
 
-    {:ok, response} = get("/projects/#{project.name}/branches").resp_body
-    |> Poison.decode
+    {:ok, response} = get("/projects/#{project.name}/branches").resp_body |>
+      Poison.decode
     assert length(response["branches"]) == 5
     assert Enum.sort(Map.keys(hd(response["branches"]))) ==
       Enum.sort(["id", "name", "inserted_at", "updated_at"])
+  end
+
+  test "get a single branch" do
+    project = Repo.insert %Project{name: "project1",
+                                   repo: "git@example.com:user/project"}
+    branch = Repo.insert %Branch{name: "branch1", exists_in_git: false,
+                                 project_id: project.id}
+
+    {:ok, response} =
+      get("/projects/#{project.name}/branches/#{branch.name}").resp_body |>
+      Poison.decode
+    assert length(response["branches"]) == 1
+    assert hd(response["branches"])["name"] == branch.name
   end
 end
