9d2007afd4e2cbf6ab14bc65630734a51f33128f
Theron Boerner <hunterboerner@gmail.com>
2015-05-13 14:00:51 -0500

    Remove browser pipeline

--- web/router.ex
+++ web/router.ex
@@ -1,13 +1,6 @@
 defmodule Rabbitci.Router do
   use Phoenix.Router
 
-  pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-  end
-
   pipeline :api do
     plug :accepts, ["json"]
     plug :allow_origin
