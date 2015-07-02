f49ecfc2a159c8e8c726b46185da49344ec34b94
Theron Boerner <hunterboerner@gmail.com>
2015-07-01 21:32:11 -0500

    Make this repo an umbrella app

---
+++ apps/rabbitci_core/mix.exs
@@ -0,0 +1,49 @@
+defmodule RabbitCICore.Mixfile do
+  use Mix.Project
+
+  def project do
+    [app: :rabbitci_core,
+     version: "0.0.1",
+     elixir: "~> 1.0",
+     elixirc_paths: elixirc_paths(Mix.env),
+     deps_path: "../../deps",
+     lockfile: "../../mix.lock",
+     compilers: [:phoenix] ++ Mix.compilers,
+     build_embedded: Mix.env == :prod,
+     start_permanent: Mix.env == :prod,
+     test_coverage: [tool: ExCoveralls],
+     deps: deps]
+  end
+
+  # Configuration for the OTP application
+  #
+  # Type `mix help compile.app` for more information
+  def application do
+    [mod: {RabbitCICore, []},
+     applications: [:phoenix, :cowboy, :logger, :ecto, :postgrex, :exq,
+                    :phoenix_ecto]]
+  end
+
+  # Specifies your project dependencies
+  #
+  # Type `mix help deps` for examples and options
+  defp deps do
+    [{:phoenix, "~> 0.13.0"},
+     {:phoenix_ecto, "~> 0.5.0"},
+     {:postgrex, "~> 0.8.0"},
+     {:cowboy, "~> 1.0"},
+     {:postgrex, "~> 0.8.0"},
+     {:ecto, "~> 0.12.0"},
+     {:ashes, ">= 0.0.3"},
+     {:mock, "~> 0.1.1"},
+     {:exq, github: "akira/exq"},
+     {:ja_serializer, github: "AgilionApps/ja_serializers"},
+     {:excoveralls, "~> 0.3.0", only: [:dev, :test]},
+     {:eredis, github: 'wooga/eredis', tag: 'v1.0.5'}
+    ]
+  end
+
+  # Specifies which paths to compile per environment
+  defp elixirc_paths(:test), do: ["lib", "web", "test/support"]
+  defp elixirc_paths(_),     do: ["lib", "web"]
+end
--- mix.exs
+++ mix.exs
@@ -1,46 +1,26 @@
-defmodule RabbitCICore.Mixfile do
+defmodule RabbitCI.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :rabbitci_core,
-     version: "0.0.1",
-     elixir: "~> 1.0",
-     elixirc_paths: elixirc_paths(Mix.env),
-     compilers: [:phoenix] ++ Mix.compilers,
+    [apps_path: "apps",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     test_coverage: [tool: ExCoveralls],
      deps: deps]
   end
 
-  # Configuration for the OTP application
+  # Dependencies can be Hex packages:
   #
-  # Type `mix help compile.app` for more information
-  def application do
-    [mod: {RabbitCICore, []},
-     applications: [:phoenix, :cowboy, :logger, :ecto, :postgrex, :exq, :phoenix_ecto]]
-  end
-
-  # Specifies your project dependencies
+  #   {:mydep, "~> 0.3.0"}
   #
-  # Type `mix help deps` for examples and options
+  # Or git/path repositories:
+  #
+  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
+  #
+  # Type `mix help deps` for more examples and options.
+  #
+  # Dependencies listed here are available only for this project
+  # and cannot be accessed from applications inside the apps folder
   defp deps do
-    [{:phoenix, "~> 0.13.0"},
-     {:phoenix_ecto, "~> 0.5.0"},
-     {:postgrex, "~> 0.8.0"},
-     {:cowboy, "~> 1.0"},
-     {:postgrex, "~> 0.8.0"},
-     {:ecto, "~> 0.12.0"},
-     {:ashes, ">= 0.0.3"},
-     {:mock, "~> 0.1.1"},
-     {:exq, github: "akira/exq"},
-     {:ja_serializer, github: "AgilionApps/ja_serializers"},
-     {:excoveralls, "~> 0.3.0", only: [:dev, :test]},
-     {:eredis, github: 'wooga/eredis', tag: 'v1.0.5'}
-    ]
+    []
   end
-
-  # Specifies which paths to compile per environment
-  defp elixirc_paths(:test), do: ["lib", "web", "test/support"]
-  defp elixirc_paths(_),     do: ["lib", "web"]
 end
