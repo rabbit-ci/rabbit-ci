dfd2c6567b4e322e7c6b85bf1c98bbc142e6dadc
Theron Boerner <hunterboerner@gmail.com>
2015-07-11 17:13:34 -0500

    [BuildMan] Add AMQP package to deps

--- apps/build_man/mix.exs
+++ apps/build_man/mix.exs
@@ -17,7 +17,7 @@ defmodule BuildMan.Mixfile do
   #
   # Type `mix help compile.app` for more information
   def application do
-    [applications: [:logger],
+    [applications: [:logger, :amqp],
      mod: {BuildMan, []}]
   end
 
@@ -35,6 +35,7 @@ defmodule BuildMan.Mixfile do
   #
   # Type `mix help deps` for more examples and options
   defp deps do
-    [{:excoveralls, "~> 0.3.0", only: [:dev, :test]}]
+    [{:excoveralls, "~> 0.3.0", only: [:dev, :test]},
+     {:amqp, "0.1.1"}]
   end
 end
