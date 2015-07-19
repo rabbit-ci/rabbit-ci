b585e0e6e997941afa7b0c49e3585c97e98ad70a
Theron Boerner <hunterboerner@gmail.com>
2015-07-19 18:59:32 -0500

    [BuildMan] FileHelpers tests

--- apps/build_man/lib/build_man/file_helpers.ex
+++ apps/build_man/lib/build_man/file_helpers.ex
@@ -1,11 +1,11 @@
 defmodule BuildMan.FileHelpers do
   def unique_folder(prefix \\ "") do
     system_tmp = System.tmp_dir
-    unique_hash = :erlang.phash2(make_ref)
-    tmp_path = "#{system_tmp}RabbitCI/#{prefix}#{unique_hash}"
+    {a, b, c} = :erlang.now
+    n = node
+    tmp_path = "#{system_tmp}RabbitCI/#{prefix}#{n}-#{a}.#{b}.#{c}"
     case File.mkdir_p(tmp_path) do
       :ok -> {:ok, tmp_path}
-      a = {:error, _} -> a
     end
   end
 end
