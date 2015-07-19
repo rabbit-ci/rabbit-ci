03a1dee02de4696d14138f07c7afb4a294fdebd9
Theron Boerner <hunterboerner@gmail.com>
2015-07-19 17:02:23 -0500

    [BuildMan] Add env specific configs

--- apps/build_man/config/config.exs
+++ apps/build_man/config/config.exs
@@ -13,10 +13,4 @@ use Mix.Config
 config :logger, :console,
 format: "\n$time $metadata[$level] $levelpad$message"
 
-# It is also possible to import configuration files, relative to this
-# directory. For example, you can emulate configuration per environment
-# by uncommenting the line below and defining dev.exs, test.exs and such.
-# Configuration from the imported file will override the ones defined
-# here (which is why it is important to import them last).
-#
-#     import_config "#{Mix.env}.exs"
+import_config "#{Mix.env}.exs"
