21b7533df752a8faaecf836661cd37c7f74b9efb
Theron Boerner <hunterboerner@gmail.com>
2015-07-20 21:59:01 -0500

    [BuildMan] FileHelpers should use Path.join not /

--- apps/build_man/lib/build_man/file_helpers.ex
+++ apps/build_man/lib/build_man/file_helpers.ex
@@ -3,7 +3,8 @@ defmodule BuildMan.FileHelpers do
     system_tmp = System.tmp_dir
     {a, b, c} = :erlang.now
     n = node
-    tmp_path = "#{system_tmp}RabbitCI/#{prefix}#{n}-#{a}.#{b}.#{c}"
+    dir_name = "#{prefix}#{n}-#{a}.#{b}.#{c}"
+    tmp_path = Path.join([system_tmp, "RabbitCI", dir_name])
     case File.mkdir_p(tmp_path) do
       :ok -> {:ok, tmp_path}
     end
