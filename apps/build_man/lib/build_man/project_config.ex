3a4073cca0436c7f8e04b636df2ef2ab378dd648
Theron Boerner <hunterboerner@gmail.com>
2015-08-14 13:29:20 -0500

    ProjectConfig is no longer a GenServer
    ProjectConfig was a GenServer because that was the only efficient way to
    connect to RabbitMQ. Because of the RabbitMQ connection pool, this is no
    longer needed.

--- apps/build_man/lib/build_man/project_config.ex
+++ apps/build_man/lib/build_man/project_config.ex
@@ -1,46 +1,20 @@
 defmodule BuildMan.ProjectConfig do
   @moduledoc """
-  GenServer for processing project configs.
+  Module for processing configs.
   """
 
-  use GenServer
-  use AMQP
-
   @exchange "rabbitci_builds_build_exchange"
-  @queue "rabbitci_builds_build_queue"
-
-  # Client API
-  def start_link do
-    GenServer.start_link(__MODULE__, :ok, name: ProjectConfigSup)
-  end
 
   @doc """
   Queue build from parsed config. See `parse_from_yaml/1` for parsing
   the config.
   """
   def queue_build(config) do
-    GenServer.cast(ProjectConfigSup, {:queue_build, config})
+    BuildMan.RabbitMQ.publish(@exchange, "", :erlang.term_to_binary(config))
   end
 
   def parse_from_yaml(content) do
     :yamerl_constr.string(content)
-  end
-
-  # Server callbacks
-  def init(:ok) do
-    {:ok, conn} = AMQP.Connection.open
-    {:ok, chan} = AMQP.Channel.open(conn)
-
-    Queue.declare(chan, @queue, durable: true)
-    Exchange.fanout(chan, @exchange, durable: true)
-    Queue.bind(chan, @queue, @exchange)
-
-    {:ok, chan}
-  end
-
-  def handle_cast({:queue_build, config}, chan) do
-    AMQP.Basic.publish chan, @exchange, "", :erlang.term_to_binary(config)
-    {:noreply, chan}
   end
 
   def queue_builds(%{"steps" => steps, "repo" => repo}) when is_list(steps) do
