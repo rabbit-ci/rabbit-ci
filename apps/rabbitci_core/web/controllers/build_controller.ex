79982710291a7cfa68699f51d1918efbc26e9015
Theron Boerner <hunterboerner@gmail.com>
2015-10-03 14:44:09 -0500

    Get rid of get_parents helper
    BuildController is stupid fast now.

--- apps/rabbitci_core/web/controllers/build_controller.ex
+++ apps/rabbitci_core/web/controllers/build_controller.ex
@@ -8,27 +8,24 @@ defmodule RabbitCICore.BuildController do
   alias RabbitCICore.Project
   alias RabbitCICore.Repo
 
-  # TODO: clean this up
-  defp get_parents(%{"project" => project_name, "branch" => branch_name}) do
-    Logger.warn "get_parents called!"
-    project = Repo.one(from p in Project, where: p.name == ^project_name)
-    branch = Repo.one(from b in Branch,
-                      where: b.name == ^branch_name and
-                      b.project_id == ^project.id)
-    {project, branch}
-  end
-
-  def index(conn, params = %{"page" => %{"offset" => page}}) do
-    {_, branch} = get_parents(params)
+  def index(conn, params = %{"branch" => branch,
+                             "project" => project,
+                             "page" => %{"offset" => page}}) do
     page = String.to_integer(page)
-    builds = Repo.all(from b in Build,
-                      where: b.branch_id == ^branch.id,
-                      limit: 30,
-                      offset: ^(page * 30),
-                      order_by: [desc: b.build_number])
+    builds =
+      (from b in Build,
+       join: br in assoc(b, :branch),
+       join: p in assoc(br, :project),
+       where: br.name == ^branch
+       and p.name == ^project,
+       limit: 30,
+       offset: ^(page * 30),
+       order_by: [desc: b.build_number],
+       preload: [:scripts, branch: {br, project: p}])
+      |> Repo.all
 
     conn
-    |> assign(:builds, Repo.preload(builds, [branch: [:project]]))
+    |> assign(:builds, builds)
     |> render("index.json")
   end
 
