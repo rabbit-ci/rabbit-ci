2cdf86eb5cce6f3f3036b9897856637a186ac279
Theron Boerner <hunterboerner@gmail.com>
2015-10-05 20:43:11 -0500

    Move RabbitMQ config to build_man app

--- apps/build_man/config/config.exs
+++ apps/build_man/config/config.exs
@@ -13,6 +13,15 @@ use Mix.Config
 config :logger, :console,
 format: "\n$time $metadata[$level] $levelpad$message"
 
-import_config "../../../config/rabbitmq.exs"
+config :build_man, :worker_limit, 2
+config :build_man, :config_extraction_limit, 2
+config :build_man, :log_streamer_limit, 10
+
+config :build_man, :build_logs_exchange, "rabbitci.build_logs"
+config :build_man, :build_exchange, "rabbitci.builds"
+config :build_man, :build_queue, "rabbitci.builds"
+config :build_man, :config_extraction_exchange, "rabbitci.config_extraction"
+config :build_man, :config_extraction_queue, "rabbitci.config_extraction"
+
 import_config "../../rabbitci_core/config/config.exs"
 import_config "#{Mix.env}.exs"
--- apps/rabbitci_core/config/config.exs
+++ apps/rabbitci_core/config/config.exs
@@ -25,7 +25,6 @@ config :plug, :mimes, %{
   "application/vnd.api+json" => ["json"]
 }
 
-import_config "../../../config/rabbitmq.exs"
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
