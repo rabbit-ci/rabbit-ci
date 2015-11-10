0f594f8cc4c7f58b0e3263bdb276ed92e2149626
Theron Boerner <hunterboerner@gmail.com>
2015-11-10 12:09:46 -0600

    Add signature checking to GitHub controller

--- apps/rabbitci_core/web/controllers/github_controller.ex
+++ apps/rabbitci_core/web/controllers/github_controller.ex
@@ -1,9 +1,11 @@
 defmodule RabbitCICore.GitHubController do
   use RabbitCICore.Web, :controller
   alias RabbitCICore.IncomingWebhooks, as: Webhooks
+  alias RabbitCICore.Project
 
-  # FIXME: Project needs a "github_secret" column first.
-  # plug :check_signature
+  plug :process_github_payload
+  plug :load_project
+  plug :check_signature
 
   # Queue a build from the GitHub push payload.
   def create(conn, params) do
@@ -11,7 +13,7 @@ defmodule RabbitCICore.GitHubController do
   end
 
   defp _create(conn, params, _event = ["PushEvent"]) do
-    case Webhooks.start_build(process_github_payload(params)) do
+    case Webhooks.start_build(conn.assigns.fixed_params) do
       {:ok, build} -> json(conn, %{message: "Success!"})
       {:error, error} -> conn |> put_status(:bad_request) |> json(error)
     end
@@ -25,21 +27,31 @@ defmodule RabbitCICore.GitHubController do
   end
 
   # Converts a map of the GitHub payload to a map with the important parts.
-  defp process_github_payload(%{"after" => commit, "ref" => ref,
-                                "repository" => %{"clone_url" => repo}}) do
-    %{branch: branch_name(ref),
-      commit: commit,
-      repo: repo}
+  defp process_github_payload(conn = %{params:
+                                       %{"after" => commit,
+                                         "ref" => ref,
+                                         "repository" =>
+                                           %{"clone_url" => repo}}}, []) do
+    assign(conn, :fixed_params, %{branch: branch_name(ref),
+                                  commit: commit,
+                                  repo: repo})
   end
 
   # Gets the branch name from the "ref" key on the payload. Example "ref" value
   # "refs/heads/changes" where "changes" is the branch name.
   defp branch_name("refs/heads/" <> name), do: name
 
+  defp load_project(conn = %{assigns:
+                             %{fixed_params: %{repo: repo}}}, []) do
+    assign(conn, :project, Repo.get_by!(Project, repo: repo))
+  end
+
   # Plug to check signature from x-hub-signature header. In the format: sha1=...
-  defp check_signature(conn, []) do
+  defp check_signature(conn = %{assigns:
+                                %{project:
+                                  %Project{webhook_secret: secret}}}, []) do
     signature = get_req_header(conn, "x-hub-signature")
-    case _check_signature(signature, conn.private.raw_body) do
+    case do_check_signature(signature, conn.private.raw_body, secret) do
       true -> conn
       false ->
         conn
@@ -49,14 +61,14 @@ defmodule RabbitCICore.GitHubController do
     end
   end
 
-  # FIXME: Project needs a "github_secret" column first.
-
-  # # Does the actual signature checking.
-  # defp _check_signature([a], body), do: _check_signature(a, body)
-  # defp _check_signature("sha1=" <> signature, body) do
-  #   :crypto.hmac(:sha, <secret goes here>, body)
-  #   |> Base.encode16
-  #   |> String.upcase == String.upcase(signature)
-  # end
-  # defp _check_signature(_, _), do: false
+  # Does the actual signature checking.
+  # If a secret has not been set, we do not allow access.
+  defp do_check_signature(_, _, nil), do: false
+  defp do_check_signature("sha1=" <> signature, body, secret) do
+    :crypto.hmac(:sha, secret, body)
+    |> Base.encode16
+    |> String.upcase == String.upcase(signature)
+  end
+  defp do_check_signature([a], b, s), do: do_check_signature(a, b, s)
+  defp do_check_signature(_, _, _), do: false
 end
