7121dce2e7d35efe3fce03504441c4178649e1cb
Theron Boerner <hunterboerner@gmail.com>
2015-12-25 10:55:48 -0600

    Fix IndexControllerTest

--- apps/rabbitci_core/web/controllers/index_controller.ex
+++ apps/rabbitci_core/web/controllers/index_controller.ex
@@ -2,6 +2,6 @@ defmodule RabbitCICore.IndexController do
   use RabbitCICore.Web, :controller
 
   def index(conn, _params) do
-    send_resp(conn, 200, "Server is running.")
+    text(conn, "Server is running.")
   end
 end
