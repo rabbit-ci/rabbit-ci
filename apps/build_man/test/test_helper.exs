d25ce9750ab10ab8790c8ea0a5e465dadf3b022a
Theron Boerner <hunterboerner@gmail.com>
2016-02-11 10:59:55 -0600

    Remove noise from test helpers

--- apps/build_man/test/test_helper.exs
+++ apps/build_man/test/test_helper.exs
@@ -3,9 +3,8 @@
 # external. You can run these tests with: `mix test --only external:true`
 ExUnit.configure(exclude: [external: true])
 ExUnit.start
-IO.puts "Creating DB and running migrations"
-Mix.Task.run "ecto.create", ["-r", "RabbitCICore.EctoRepo"]
-Mix.Task.run "ecto.migrate", ["-r", "RabbitCICore.EctoRepo"]
+Mix.Task.run "ecto.create", ["--quiet", "-r", "RabbitCICore.EctoRepo"]
+Mix.Task.run "ecto.migrate", ["--quiet", "-r", "RabbitCICore.EctoRepo"]
 Ecto.Adapters.SQL.rollback_test_transaction(RabbitCICore.EctoRepo)
 Ecto.Adapters.SQL.begin_test_transaction(RabbitCICore.EctoRepo)
 Application.ensure_all_started(:ex_machina)
--- apps/rabbitci_core/test/test_helper.exs
+++ apps/rabbitci_core/test/test_helper.exs
@@ -1,7 +1,6 @@
 ExUnit.start
-IO.puts "Creating DB and running migrations"
-Mix.Task.run "ecto.create", ["-r", "RabbitCICore.EctoRepo"]
-Mix.Task.run "ecto.migrate", ["-r", "RabbitCICore.EctoRepo"]
+Mix.Task.run "ecto.create", ["--quiet", "-r", "RabbitCICore.EctoRepo"]
+Mix.Task.run "ecto.migrate", ["--quiet", "-r", "RabbitCICore.EctoRepo"]
 Ecto.Adapters.SQL.rollback_test_transaction(RabbitCICore.EctoRepo)
 Ecto.Adapters.SQL.begin_test_transaction(RabbitCICore.EctoRepo)
 Application.ensure_all_started(:ex_machina)
