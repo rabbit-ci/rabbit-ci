6f9e80845ae102427670d22b7bec953f2273142c
Theron Boerner <hunterboerner@gmail.com>
2016-03-17 21:54:19 -0500

    Don't send null log attribute when no_logs is true

--- apps/rabbitci_core/web/serializers/step_serializer.ex
+++ apps/rabbitci_core/web/serializers/step_serializer.ex
@@ -16,6 +16,14 @@ defmodule RabbitCICore.StepSerializer do
 
   attributes [:name, :status, :log, :start_time, :finish_time]
 
+  def attributes(step, conn) do
+    attrs = super(step, conn)
+    case conn.assigns[:no_logs] do
+      true -> Map.drop(attrs, [:log])
+      _ -> attrs
+    end
+  end
+
   def type, do: "steps"
   def log(r, %Plug.Conn{assigns: %{no_logs: true}}), do: nil
   def log(r, _) do
