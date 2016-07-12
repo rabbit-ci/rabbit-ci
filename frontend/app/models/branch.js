0610f744f4d6702b1176c0d6827948df688b9203
Theron Boerner <hunterboerner@gmail.com>
2016-07-12 15:53:25 -0500

    Running and queued builds count

--- frontend/app/models/branch.js
+++ frontend/app/models/branch.js
@@ -6,5 +6,13 @@ export default DS.Model.extend({
   builds: DS.hasMany('build'),
 
   buildsSorting: ['buildNumber:desc'],
-  sortedBuilds: Ember.computed.sort('builds', 'buildsSorting')
+  sortedBuilds: Ember.computed.sort('builds', 'buildsSorting'),
+
+  runningBuildsCount: Ember.computed('builds.@each.status', function() {
+    return this.get('builds').filterBy('status', 'running').get('length');
+  }),
+
+  queuedBuildsCount: Ember.computed('builds.@each.status', function() {
+    return this.get('builds').filterBy('status', 'queued').get('length');
+  })
 });
